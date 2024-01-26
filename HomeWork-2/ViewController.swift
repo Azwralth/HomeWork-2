4872398854741c69f2a9a9e6974d9121729e1e8e
Vlad <mackva34@gmail.com>
2024-01-27 00:59:49 +0100

    working out the color and button change logic

--- HomeWork-2/ViewController.swift
+++ HomeWork-2/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greetingButton: UIButton!
+    
+    private var currentColor = "red"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        greetingButton.layer.cornerRadius = 10
+        redView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
     }
-
-
+    
+    @IBAction func changeColor() {
+        greetingButton.setTitle("NEXT", for: .normal)
+        switch currentColor {
+        case "red":
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+            currentColor = "yellow"
+        case "yellow":
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+            currentColor = "green"
+        case "green":
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentColor = "red"
+        default:
+            break
+        }
+    }
 }
 
